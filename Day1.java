e38752b8257f9c87d1911f013b830186a36224cc
trendingeswari <trendingeswari@gmail.com>
2024-12-10 16:20:04 +0530

    Day1

---
+++ Day1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Day1 {
+    public static void main(String[] args) {
+		//Your Code Here
+        Scanner sc = new Scanner (System.in);
+        int arr[]=new int[10];
+        for(int i=0;i<10;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        sc.nextLine();
+        String S[]=sc.nextLine().split("\\s+");
+        int val[]=new int[10];
+        for(int i=0;i<S.length;i++)
+        {
+            int num=Integer.parseInt(S[i]);
+            while(num>0)
+            {
+                val[num%10]++;
+                num/=10;
+            }
+        }
+        for(int i=0;i<10;i++)
+        {
+            if(arr[i]<val[i])
+            {
+                System.out.print("NO");
+                return ;
+            }
+        }
+        sc.close();
+        System.out.print("YES");
+	}
+}
